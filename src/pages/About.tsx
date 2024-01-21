13f4a10a85bdbdea54377c385d1252c7b5dccb48
Toby Portelli <tobyportelli@gmail.com>
2024-01-21 15:01:47 -0500

    Added navigation links

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const About: React.FC = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div>About</div>
+      <Link to="/">Homepage</Link>
+    </>
+  );
 };
 export default About;
--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PageNotFound: React.FC = () => {
-  return <div>404 PAGE NOT FOUND</div>;
+  return (
+    <>
+      <div>404 PAGE NOT FOUND</div>
+      <Link to="/">Return to Homepage</Link>
+    </>
+  );
 };
 export default PageNotFound;
